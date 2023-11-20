36191e7332bfc53994a6b7de10d807f165a6fc54
Claus Jespersen <46089765+clajes@users.noreply.github.com>
2023-11-20 12:29:33 +0100

    Update M365TenantConfig.ps1
    removed module version on import-dscresource command

--- ConditionalAccessSamplePolicies/Microsoft365DSCCAGroups/M365TenantConfig.ps1
+++ ConditionalAccessSamplePolicies/Microsoft365DSCCAGroups/M365TenantConfig.ps1
@@ -25,7 +25,7 @@ Configuration M365TenantConfig
     }
 
     $OrganizationName = $CredsCredential.UserName.Split('@')[1]
-    Import-DscResource -ModuleName 'Microsoft365DSC' -ModuleVersion '1.23.1011.1'
+    Import-DscResource -ModuleName 'Microsoft365DSC' 
 
     Node localhost
     {
