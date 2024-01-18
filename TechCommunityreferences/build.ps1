25dc158bb53833fca3d8cc1ccce06a17317ef6f7
Claus Jespersen <46089765+clajes@users.noreply.github.com>
2024-01-18 14:31:49 +0100

    Update build.ps1
    corrected name of powershell script from ConfigureMicrosoft365.ps1 to ConfigureCAPolicy.ps1 as this is the name of the powershell script in the repo. Thanks to Tom Camps for this input

--- TechCommunityreferences/build.ps1
+++ TechCommunityreferences/build.ps1
@@ -7,4 +7,4 @@ $m365configcertStringBase64 = $m365cert
 $m365configcertByteArray = [System.Convert]::FromBase64String($m365configcertStringBase64) 
 $m365configcert = [System.Security.Cryptography.X509Certificates.X509Certificate2]($m365configcertByteArray) 
 $m365configcertThumbprint = $m365configcert.Thumbprint 
-. .\ConfigureMicrosoft365.ps1 -CertificateThumbprint $m365configcertThumbprint -TenantId $env:TenantId -ApplicationId $env:ApplicationId 
+. .\ConfigureMCApolicy.ps1 -CertificateThumbprint $m365configcertThumbprint -TenantId $env:TenantId -ApplicationId $env:ApplicationId 
